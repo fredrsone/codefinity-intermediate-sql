74c9a31edd76eef267aac0fad2cf20d0960cfa4f
fredrsone <>
2026-08-13 20:35:01 +0000

    Challenge: Get Product Amount by Category

--- section/challenge_get_product_amount_by_category/query.sql
+++ section/challenge_get_product_amount_by_category/query.sql
@@ -1,8 +1,8 @@
 --find the product names and their quantities in stock. Join two tables: product and category.
 --Focus on products in the Meat or Grains categories. Use the OR operator for these categories and ensure the product quantity is less than 100
-SELECT product.name AS product_name, product.amount AS product_amount
+SELECT product.name AS product_name, product.amount  AS amount
 FROM product
 JOIN category 
     ON product.category_id = category.id
-WHERE category.name = 'Meat' OR category.name = 'Grains' AND  product.amount < 100
+WHERE (category.name = 'Meat' OR category.name = 'Grains') AND  product.amount < 100
 ORDER BY product.name;
